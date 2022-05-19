7bd81c26e700358651655f8fd122d6bb5802b349
Charles Tang <75450612+charlestang06@users.noreply.github.com>
2022-05-19 14:14:00 +0000

    colorful circles!

--- Circle.java
+++ Circle.java
@@ -5,6 +5,9 @@ public class Circle extends JComponent {
 	public int diameter;
 	public int x;
 	public int y;
+	private Color[] colors ;
+	private int rand;
+
 
 	/**
 	 * @param diameter of the circle
@@ -17,6 +20,8 @@ public class Circle extends JComponent {
 		this.diameter = d;
 		this.x = x;
 		this.y = y;
+		colors = new Color[] {Color.MAGENTA, Color.BLUE, Color.GREEN, Color.YELLOW, Color.ORANGE, Color.RED};
+		rand =  (int) (Math.random() * colors.length);
 	}
 
 	/**
@@ -29,14 +34,14 @@ public class Circle extends JComponent {
 	/**
 	 * @return the x-center of the circle
 	 */
-	public int getXCenter() {
+	public int getXCoord() {
 		return x;
 	}
 
 	/**
 	 * @return the y-center of the circle
 	 */
-	public int getYCenter() {
+	public int getYCoord() {
 		return y;
 	}
 
@@ -47,7 +52,7 @@ public class Circle extends JComponent {
 	public void paintComponent(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.drawOval(x, y, diameter, diameter);
-		g2d.setColor(Color.BLUE);
+				g2d.setColor(colors[rand]);
 		g2d.fillOval(x, y, diameter, diameter);
 	}
 }
